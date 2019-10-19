510de0c2c93adc78f8635de1e0fcae9eb32ecb8d
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-10-19 20:57:21 +0900

    feat: add hnst and vapart

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main // github.com/ykzts/aaa
+package main // import "github.com/ykzts/aaa"
 
 import (
 	"log"
@@ -9,6 +9,98 @@ import (
 	"github.com/ChimeraCoder/anaconda"
 	influxdb "github.com/influxdata/influxdb1-client/v2"
 )
+
+var targets = []struct {
+	ID         int64
+	ScreenName string
+	Group      string
+}{
+	{
+		992355895051866112,
+		"_kanade_kanon",
+		"VApArt",
+	},
+	{
+		993839162099810305,
+		"AniMare_cafe",
+		"AniMare",
+	},
+	{
+		388327297,
+		"camomi_camomi",
+		"VApArt",
+	},
+	{
+		1007316805981892613,
+		"Charlotte_HNST",
+		"HoneyStrap",
+	},
+	{
+		1007290563756871681,
+		"Eli_HNST",
+		"HoneyStrap",
+	},
+	{
+		995247053977485313,
+		"Haneru_Inaba",
+		"AniMare",
+	},
+	{
+		1007247110167674881,
+		"HNST_official",
+		"HoneyStrap",
+	},
+	{
+		995250952373391360,
+		"Hinako_Umori",
+		"AniMare",
+	},
+	{
+		995253301472972801,
+		"Ichika_Souya",
+		"AniMare",
+	},
+	{
+		1007331251685023744,
+		"Mary_HNST",
+		"HoneyStrap",
+	},
+	{
+		1007339408343777281,
+		"Mico_HNST",
+		"HoneyStrap",
+	},
+	{
+		1007268292648505344,
+		"Patra_HNST",
+		"HoneyStrap",
+	},
+	{
+		995258822556991493,
+		"Ran_Hinokuma",
+		"AniMare",
+	},
+	{
+		1091649332539846656,
+		"Uge_And",
+		"VApArt",
+	},
+	{
+		1091634367158337536,
+		"Vtuber_ApArt",
+		"VApArt",
+	},
+}
+
+func getGroup(id int64) string {
+	for _, target := range targets {
+		if target.ID == id {
+			return target.Group
+		}
+	}
+
+	return ""
+}
 
 func main() {
 	conf := influxdb.HTTPConfig{
@@ -35,13 +127,19 @@ func main() {
 		os.Getenv("TWITTER_CONSUMER_SECRET"),
 	)
 
-	for _, n := range []string{"Haneru_Inaba", "Hinako_Umori", "Ichika_Souya", "Ran_Hinokuma"} {
-		user, err := api.GetUsersShow(n, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
+	var ids []int64
+	for _, t := range targets {
+		ids = append(ids, t.ID)
+	}
 
+	users, err := api.GetUsersLookupByIds(ids, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, user := range users {
 		tags := map[string]string{
+			"group":       getGroup(user.Id),
 			"id":          user.IdStr,
 			"screen_name": user.ScreenName,
 		}
