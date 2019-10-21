ab1ec9cd5651472c228b1587b95d898f06944ab9
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-10-21 19:09:39 +0900

    feat: dockernize (#2)

--- worker/go.mod
+++ worker/go.mod
@@ -1,4 +1,4 @@
-module github.com/ykzts/aaa
+module github.com/ykzts/graph/worker
 
 go 1.13
 
--- worker/main.go
+++ worker/main.go
@@ -1,4 +1,4 @@
-package main // import "github.com/ykzts/aaa"
+package main // import "github.com/ykzts/graph/worker"
 
 import (
 	"log"
