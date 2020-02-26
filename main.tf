1edb90cb844ffea3dbf1e20897ce86445e099380
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-02-26 22:26:58 +0900

    chore: configure terraform (#52)

---
+++ main.tf
@@ -0,0 +1,49 @@
+terraform {
+  backend "remote" {}
+}
+
+provider "google" {
+  credentials = var.credentials
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "${local.name}-cluster"
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${local.name}-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = var.machine_type
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
