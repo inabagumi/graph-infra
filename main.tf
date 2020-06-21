90add8b0b7d4ccac0a64bc40250cc52ecce053f1
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-06-22 02:49:34 +0900

    fix(terraform): move node_version to node pool (#143)
    * fix(terraform): move node_version to node pool
    
    * fix(terraform): fix attribute name

--- main.tf
+++ main.tf
@@ -30,9 +30,9 @@ resource "google_dns_record_set" "frontend" {
 }
 
 resource "google_container_cluster" "primary" {
-  name         = "${local.name}-cluster"
-  location     = var.region
-  node_version = "1.15.12-gke.2"
+  name               = "${local.name}-cluster"
+  location           = var.region
+  min_master_version = "1.15.12-gke.2"
 
   node_locations = [
     var.zone
@@ -57,6 +57,7 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "${local.name}-node-pool"
   location   = google_container_cluster.primary.location
   cluster    = google_container_cluster.primary.name
+  version    = "1.15.12-gke.2"
   node_count = 2
 
   autoscaling {
