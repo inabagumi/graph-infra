81bfcc2c952e5aff3960ff04005302e8a2cf75a4
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-06-26 00:26:02 +0900

    feat(terraform): add new cluster (#147)
    * feat(terraform): add new cluster
    
    * fix(terraform): fix name

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,6 +53,26 @@ resource "google_container_cluster" "primary" {
   ip_allocation_policy {}
 }
 
+resource "google_container_cluster" "main" {
+  name     = local.name
+  location = var.zone
+
+  min_master_version       = "1.16.9-gke.6"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+
+  ip_allocation_policy {}
+}
+
 resource "google_container_node_pool" "primary_nodes" {
   name       = "${local.name}-node-pool"
   location   = google_container_cluster.primary.location
@@ -73,6 +93,40 @@ resource "google_container_node_pool" "primary_nodes" {
   node_config {
     preemptible  = true
     machine_type = "n1-standard-1"
+    disk_size_gb = 10
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+
+resource "google_container_node_pool" "main_nodes" {
+  name       = "${local.name}-node-pool"
+  location   = google_container_cluster.main.location
+  cluster    = google_container_cluster.main.name
+  version    = google_container_cluster.main.min_master_version
+  node_count = 2
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 5
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-small"
     disk_size_gb = 10
 
     metadata = {
