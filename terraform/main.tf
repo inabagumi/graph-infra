bf3f8878f0f5bbd28f4b7f150362f4cd6ccb2f36
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-06-26 02:52:19 +0900

    fix(terraform): remove unused cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,30 +29,6 @@ resource "google_dns_record_set" "frontend" {
   rrdatas = [google_compute_global_address.default.address]
 }
 
-resource "google_container_cluster" "primary" {
-  name               = "${local.name}-cluster"
-  location           = var.region
-  min_master_version = "1.16.9-gke.6"
-
-  node_locations = [
-    var.zone
-  ]
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  master_auth {
-    username = ""
-    password = ""
-
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-
-  ip_allocation_policy {}
-}
-
 resource "google_container_cluster" "main" {
   name     = local.name
   location = var.zone
@@ -71,40 +47,6 @@ resource "google_container_cluster" "main" {
   }
 
   ip_allocation_policy {}
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "${local.name}-node-pool"
-  location   = google_container_cluster.primary.location
-  cluster    = google_container_cluster.primary.name
-  version    = google_container_cluster.primary.min_master_version
-  node_count = 2
-
-  autoscaling {
-    min_node_count = 1
-    max_node_count = 5
-  }
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    preemptible  = true
-    machine_type = "n1-standard-1"
-    disk_size_gb = 10
-
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
 }
 
 resource "google_container_node_pool" "main_nodes" {
