43eafa43153151530e422434e0868347bfd47a01
Yamagishi Kazutoshi <ykzts@haneru.dev>
2022-06-12 10:48:05 +0900

    fix(terraform): remove legacy params

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,9 +37,6 @@ resource "google_container_cluster" "main" {
   initial_node_count       = 1
 
   master_auth {
-    username = ""
-    password = ""
-
     client_certificate_config {
       issue_client_certificate = false
     }
