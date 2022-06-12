a2387eb726aac081e1ffd61be01fef6cd6e47f88
Yamagishi Kazutoshi <ykzts@haneru.dev>
2022-06-12 10:50:40 +0900

    fix(terraform): remove legacy params (#372)
    * fix(terraform): remove legacy params
    
    * fix(terraform): add enable_shielded_nodes

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,13 +33,11 @@ resource "google_container_cluster" "main" {
   name     = local.name
   location = var.zone
 
+  enable_shielded_nodes    = false
   remove_default_node_pool = true
   initial_node_count       = 1
 
   master_auth {
-    username = ""
-    password = ""
-
     client_certificate_config {
       issue_client_certificate = false
     }
