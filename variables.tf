326ec418487c0f14ec53c1aab025e668b18dab34
Yamagishi Kazutoshi <ykzts@haneru.dev>
2022-06-13 14:45:09 +0000

    feat: migrate credentials

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "google_api_key" {
+  type = string
+}
+
+variable "influx_token" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "region" {
+  default = "asia-northeast1"
+  type    = string
+}
+variable "repo_name" {
+  default = "graph-infra"
+  type    = string
+}
+
+variable "repo_owner" {
+  default = "inabagumi"
+  type    = string
+}
+
+variable "twitter_access_token" {
+  type = string
+}
+
+variable "twitter_access_token_secret" {
+  type = string
+}
+
+variable "twitter_consumer_key" {
+  type = string
+}
+
+variable "twitter_consumer_secret" {
+  type = string
+}
+
